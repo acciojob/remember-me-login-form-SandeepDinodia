ff2295bd5e36d271fd66f6748d8c9dd1aadccd48
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-08-30 15:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+ const username=document.getElementById('username');
+ const password=document.getElementById('password');
+ const submit=document.getElementById('submit');
+ const checkbox=document.getElementById('checkbox');
+
+submit.addEventListener('click',function(event){
+	event.preventDefault();
+	if(username.value != '' && password.value != ''){
+		if(checkbox.checked){
+			localStorage.setItem('username',username.value);
+			localStorage.setItem('password',password.value);
+		}else{
+			localStorage.removeItem('username')
+			localStorage.removeItem('password')
+		}
+		alert("logged in as : " + username.value);
+	}
+});
+if(localStorage.getItem('username')) {
+	existing.style.display = 'block';
+	existing.addEventListener('click', function() {
+		alert("Logged in as " + localStorage.getItem('username'));
+	});
+} else {
+	existing.style.display = 'none';
+}
